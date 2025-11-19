787f5402e06a7e078c72a4e35e40afc102246f7a
Tharindu Prabath <116081100+tharindu899@users.noreply.github.com>
2025-11-19 14:04:01 +0530

    update sw.js

--- sw.js
+++ sw.js
@@ -28,3 +28,20 @@ self.addEventListener('fetch', e => {
     })
   );
 });
+
+// Unregister old service worker
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(() => self.registration.unregister())
+  );
+});
